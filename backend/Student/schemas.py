94712395c9e24aa81357c1470729c96b16428b09
Mahishwara <kmisha043@mail.ru>
2025-03-08 09:28:59 +0500

    К модели студента добавляем схемы добавления и изменения данных

---
+++ backend/Student/schemas.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+
+
+class SStudent(BaseModel):
+    class Config:
+        from_attributes = True
+
+    id: int
+    firstname: str = Field(..., min_length=3, max_length=25, description="Имя, от 3 до 25 символов")
+    lastname: str = Field(..., min_length=3, max_length=35, description="Фамилия, от 3 до 35 символов")
+    skill: str = Field(..., description="Укажите навык из предложенных")
+    description: str = Field(..., description="Дополнительная информация")
+
+
+class SStudentAdd(BaseModel):
+    firstname: str = Field(..., min_length=3, max_length=25, description="Имя, от 3 до 25 символов")
+    lastname: str = Field(..., min_length=3, max_length=35, description="Фамилия, от 3 до 35 символов")
+    skill: str = Field(..., description="Укажите навык из предложенных")
+    description: str = Field(..., description="Дополнительная информация")
+
+
+
+class SCategoryUpd(BaseModel):
+    firstname: str = Field(..., min_length=3, max_length=25, description="Новое имя, от 3 до 25 символов")
+    lastname: str = Field(..., min_length=3, max_length=35, description="Новая фамилия, от 3 до 35 символов")
+    skill: str = Field(..., description="Укажите новый навык из предложенных")
+    description: str = Field(..., description="Новая дополнительная информация")
