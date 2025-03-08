4c72375b89b864d93a09fc193dea1c43789acbbd
Mahishwara <kmisha043@mail.ru>
2025-03-08 10:24:57 +0500

    Создается модель работодателя с описанием шаблонов добавления, изменения, и полей фильтрации

---
+++ backend/Employer/models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Employer(Base):
+    __tablename__ = 'Employers'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    organization: Mapped[str] = mapped_column(String(100), nullable=False)
+    email: Mapped[str] = mapped_column(String(60), nullable=True)
+    phone: Mapped[str] = mapped_column(String(20), nullable=True)
+    description: Mapped[str] = mapped_column(String(100), nullable=True)
+    extend_existing = True
+
+    def __str__(self):
+        return (f"{self.__class__.__name__}(id={self.id},"
+                f"name={self.name!r}, "
+                f"organization={self.organization!r}, "
+                f"email={self.email!r},"
+                f"phone={self.phone!r},"
+                f"adress={self.adress!r}, "
+                f"description={self.description!r})")
+
+    def __repr__(self):
+        return str(self)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "organization": self.organization,
+            "email": self.email,
+            "phone": self.phone,
+            "adress": self.adress,
+            "description": self.description,}
