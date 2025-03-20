97127b10d73bf792eb8c5d9f9c3c609ac76e7d5b
Mahishwara <kmisha043@mail.ru>
2025-03-20 11:17:34 +0500

    Добавление валидации для сущности сообщение

--- backend/Message/schemas.py
+++ backend/Message/schemas.py
@@ -8,12 +8,12 @@ class SMessage(BaseModel):
     id: int
     id_student: int = Field(..., description="Уникальный код получателя")
     id_vacancy: int = Field(..., description="Уникальный код отправителя")
-    description: str = Field(..., description="Сообщение")
+    description: str = Field(..., min_length=10, max_length=100, description="Сообщение")
     path_type: int = Field(..., description="Путь сообщения: 1 = студент-вакансия; 2 = вакансия-студент")
 
 
 class SMessageAdd(BaseModel):
     id_student: int = Field(..., description="Уникальный код получателя")
     id_vacancy: int = Field(..., description="Уникальный код отправителя")
-    description: str = Field(..., description="Сообщение")
+    description: str = Field(..., min_length=10, max_length=100, description="Сообщение")
     path_type: int = Field(..., description="Путь сообщения: 1 = студент-вакансия; 2 = вакансия-студент")
